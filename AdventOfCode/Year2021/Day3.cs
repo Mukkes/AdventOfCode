2d6ae888004e8cd0d0880a96c027743302fdc3ff
Marcel Zwart <marcel__zwart@hotmail.com>
2021-12-02 20:04:08 +0100

    Add day 3.

---
+++ AdventOfCode/Year2021/Day3.cs
@@ -0,0 +1,21 @@
+﻿namespace AdventOfCode.Year2021
+{
+    public class Day3 : PuzzleSolution
+    {
+        public override int Year => 2021;
+
+        public override int Day => 3;
+
+        //private string[] Input => GetInputAsStringArray();
+
+        public override int ResultPartOne()
+        {
+            return 0;
+        }
+
+        public override int ResultPartTwo()
+        {
+            return 0;
+        }
+    }
+}
