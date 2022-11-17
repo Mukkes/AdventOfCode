77045221311538598a5ab78f4f24d6f9e3b05818
Marcel Zwart <marcelzwart92@gmail.com>
2022-11-17 21:57:20 +0100

    Solution day 19 (not working).

---
+++ Day19/Parsers/BeaconScannerParser.cs
@@ -0,0 +1,32 @@
+﻿using AdventOfCode2021.Day16.Parsers;
+using AdventOfCode2021.Day18.Parsers;
+
+namespace AdventOfCode2021.Day19.Parsers
+{
+    public class BeaconScannerParser : InputToStringArrayParser, IInputParser<List<Scanner>>
+    {
+        public new List<Scanner> Parse(string input)
+        {
+            var scanners = new List<Scanner>();
+            foreach (var s in base.Parse(input))
+            {
+                if (s == string.Empty)
+                {
+                    continue;
+                }
+                if (s[0..2] == "--")
+                {
+                    var scannerInfo = s.Split(' ');
+                    scanners.Add(new Scanner(int.Parse(scannerInfo[2])));
+                }
+                else
+                {
+                    var beaconInfo = s.Split(',');
+                    var beacon = new Beacon(int.Parse(beaconInfo[0]), int.Parse(beaconInfo[1]), int.Parse(beaconInfo[2]));
+                    scanners.Last().Beacons.Add(beacon);
+                }
+            }
+            return scanners;
+        }
+    }
+}
---
+++ Day19/Solver/BeaconScannerSolver.cs
@@ -0,0 +1,10 @@
+﻿using AdventOfCode2021.Day16.Solvers;
+using AdventOfCode2021.Day19.Parsers;
+
+namespace AdventOfCode2021.Day19.Solver
+{
+    public abstract class BeaconScannerSolver : BaseSolver<List<Scanner>>
+    {
+        public BeaconScannerSolver(string? input) : base(new BeaconScannerParser(), input) { }
+    }
+}
