653ff8bf62b8029191842ec769fb8940ccad226b
Marcel Zwart <marcelzwart92@gmail.com>
2023-12-06 17:45:55 +0100

    Solution year 2023 day 6.

---
+++ AdventOfCode/Year2023/Day06/Solvers/Solver.cs
@@ -0,0 +1,55 @@
+﻿using AdventOfCodeLibrary.Attributes;
+using AdventOfCodeLibrary.Solvers;
+
+namespace AdventOfCode.Year2023.Day06.Solvers;
+
+[Solver]
+public class Solver : StringArraySolver
+{
+    public override int Year => 2023;
+
+    public override int Day => 06;
+
+    public override object SolvePartOne()
+    {
+        var time = Array.ConvertAll(Input[0][11..].Split(' ', StringSplitOptions.RemoveEmptyEntries), int.Parse);
+        var distance = Array.ConvertAll(Input[1][11..].Split(' ', StringSplitOptions.RemoveEmptyEntries), int.Parse);
+        var beatAmount = new int[time.Length];
+
+        for (var i = 0; i < time.Length; i++)
+        {
+            for (var j = 0; j <= time[i]; j++)
+            {
+                if (j * (time[i] - j) > distance[i])
+                {
+                    beatAmount[i]++;
+                }
+            }
+        }
+        var sum = beatAmount[0];
+        for (var i = 1; i < beatAmount.Length; i++)
+        {
+            sum *= beatAmount[i];
+        }
+        return sum;
+    }
+
+    public override object SolvePartTwo()
+    {
+        var time = new long[] { long.Parse(Input[0][11..].Replace(" ", "")) };
+        var distance = new long[] { long.Parse(Input[1][11..].Replace(" ", "")) };
+        var beatAmount = new long[time.Length];
+
+        for (var i = 0L; i < time.Length; i++)
+        {
+            for (var j = 0L; j <= time[i]; j++)
+            {
+                if (j * (time[i] - j) > distance[i])
+                {
+                    beatAmount[i]++;
+                }
+            }
+        }
+        return beatAmount[0];
+    }
+}
