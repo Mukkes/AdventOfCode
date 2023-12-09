0e1db0888d957de528937889c21a3312910ad5c4
Marcel Zwart <marcelzwart92@gmail.com>
2023-12-09 13:59:58 +0100

    Solution year 2023 day 9 part 1.

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -2,19 +2,12 @@
 using AdventOfCodeLibrary.Models;
 
 var solverRegister = new SolverRegister(typeof(Program).Assembly);
-var yearDay = new YearDay(2023, 8);
+var yearDay = new YearDay(2023, 9);
 var solver = solverRegister.Solvers.GetValueOrDefault(yearDay);
 
-var exampleInput = @"LR
-
-11A = (11B, XXX)
-11B = (XXX, 11Z)
-11Z = (11B, XXX)
-22A = (22B, XXX)
-22B = (22C, 22C)
-22C = (22Z, 22Z)
-22Z = (22B, 22B)
-XXX = (XXX, XXX)";
+var exampleInput = @"0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45";
 
 //solver.SetInput(exampleInput);
 Console.WriteLine(solver.SolvePartOne());
---
+++ AdventOfCode/Year2023/Day09/Solvers/Solver.cs
@@ -0,0 +1,63 @@
+﻿using AdventOfCodeLibrary.Attributes;
+using AdventOfCodeLibrary.Solvers;
+
+namespace AdventOfCode.Year2023.Day09.Solvers;
+
+[Solver]
+public class Solver : StringArraySolver
+{
+    public override int Year => 2023;
+
+    public override int Day => 09;
+
+    public override object SolvePartOne()
+    {
+        var sum = 0L;
+        foreach (var line in Input)
+        {
+            var history = new List<long>();
+            var values = line.Split(' ');
+            foreach (var value in values)
+            {
+                history.Add(long.Parse(value));
+            }
+            sum += GetNextValue(history);
+        }
+        return sum;
+    }
+
+    public override object SolvePartTwo()
+    {
+        return 0;
+    }
+
+    private long GetNextValue(List<long> values)
+    {
+        var differences = new List<long>();
+        for (var i = 1; i < values.Count; i++)
+        {
+            differences.Add(values[i] - values[i - 1]);
+        }
+        if (AllZero(differences))
+        {
+            return 0 + values.Last();
+        }
+        else
+        {
+
+            return GetNextValue(differences) + values.Last();
+        }
+    }
+
+    private bool AllZero(List<long> values)
+    {
+        foreach (var value in values)
+        {
+            if (value != 0L)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
