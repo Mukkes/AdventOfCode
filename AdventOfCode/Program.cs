819bf8b1062d808070cd9e482ad8f9ebe532b7d4
Marcel Zwart <marcelzwart92@gmail.com>
2024-11-17 12:42:23 +0100

    Add stopwatch.

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,10 +1,28 @@
 ﻿using AdventOfCodeLibrary;
+using System.Diagnostics;
 
 var solverRegister = new SolverRegister();
 var solver = solverRegister.Single(solver => solver.Year == 2021 && solver.Day == 19);
 
 var exampleInput = @"";
 //solver.Input = exampleInput;
+
+var stopwatch = new Stopwatch();
+stopwatch.Start();
+
 Console.WriteLine(solver.SolvePartOne());
+
+stopwatch.Stop();
+var timeSpanPartOne = stopwatch.Elapsed;
+stopwatch.Start();
+
 Console.WriteLine(solver.SolvePartTwo());
+
+stopwatch.Stop();
+var timeSpanPartTwo = stopwatch.Elapsed - timeSpanPartOne;
+var timeSpanTotal = stopwatch.Elapsed;
+
+Console.WriteLine("Part one: " + timeSpanPartOne);
+Console.WriteLine("Part two: " + timeSpanPartTwo);
+Console.WriteLine("Total: " + timeSpanTotal);
 Console.ReadKey();
