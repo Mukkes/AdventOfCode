f971c71814d4253035f13b5e162b40c484543248
Marcel Zwart <marcelzwart92@gmail.com>
2024-12-02 16:40:25 +0100

    Solution year 2024 day 2.

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -2,14 +2,14 @@
 using System.Diagnostics;
 
 var solverRegister = new SolverRegister(["AdventOfCode.dll"]);
-var solver = solverRegister.Solvers.Single(solver => solver.Year == 2024 && solver.Day == 1);
+var solver = solverRegister.Solvers.Single(solver => solver.Year == 2024 && solver.Day == 2);
 
-var exampleInput = @"3   4
-4   3
-2   5
-1   3
-3   9
-3   3";
+var exampleInput = @"7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9";
 //solver.Input = exampleInput;
 
 var stopwatch = new Stopwatch();
---
+++ AdventOfCode/Year2024/Day02/Solvers/Solver.cs
@@ -0,0 +1,101 @@
+﻿using AdventOfCodeLibrary.Attributes;
+using AdventOfCodeLibrary.Parsers;
+using AdventOfCodeLibrary.Solvers;
+
+namespace AdventOfCode.Year2024.Day02.Solvers;
+
+[Solver]
+public class Solver : BaseSolver<string[]>
+{
+    public override int Year => 2024;
+
+    public override int Day => 2;
+
+    //public override object? AnswerPartOne => ;
+
+    //public override object? AnswerPartTwo => ;
+
+    protected override IInputParser<string[]> InputParser => new StringArrayParser();
+
+    public override object SolvePartOne()
+    {
+        var safeReports = 0;
+        foreach (var report in ParsedInput)
+        {
+            var levels = Array.ConvertAll(report.Split(' '), int.Parse).ToList();
+            if (IsReportSafe(levels))
+            {
+                safeReports++;
+            }
+        }
+        return safeReports;
+    }
+
+    public override object SolvePartTwo()
+    {
+        var safeReports = 0;
+        foreach (var report in ParsedInput)
+        {
+            var levels = Array.ConvertAll(report.Split(' '), int.Parse).ToList();
+            if (IsReportSafe(levels))
+            {
+                safeReports++;
+            }
+            else
+            {
+                for (var i = 0; i < levels.Count; i++)
+                {
+                    var clone = new List<int>(levels);
+                    clone.RemoveAt(i);
+                    if (IsReportSafe(clone))
+                    {
+                        safeReports++;
+                        break;
+                    }
+                }
+            }
+        }
+        return safeReports;
+    }
+
+    private bool IsReportSafe(List<int> report)
+    {
+        for (var i = 1; i < report.Count; i++)
+        {
+            var difference = int.Abs(report[i - 1] - report[i]);
+            if (difference <= 0 || difference > 3)
+            {
+                return false;
+            }
+        }
+        if (AllIncreasing(report) || AllDecreasing(report))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private bool AllIncreasing(List<int> report)
+    {
+        for (var i = 1; i < report.Count; i++)
+        {
+            if (report[i - 1] <= report[i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private bool AllDecreasing(List<int> report)
+    {
+        for (var i = 1; i < report.Count; i++)
+        {
+            if (report[i - 1] >= report[i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
