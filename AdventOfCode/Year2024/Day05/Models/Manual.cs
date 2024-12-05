a2d3fafc092d43ca03272889f8a980e3fa877b00
Marcel Zwart <marcelzwart92@gmail.com>
2024-12-05 15:57:16 +0100

    Solution year 2024 day 5 part 1.

---
+++ AdventOfCode/Year2024/Day05/Models/Manual.cs
@@ -0,0 +1,6 @@
+﻿namespace AdventOfCode.Year2024.Day05.Models;
+public class Manual
+{
+    public HashSet<(int, int)> Rules { get; set; } = new HashSet<(int, int)>();
+    public List<List<int>> Updates { get; set; } = new List<List<int>>();
+}
---
+++ AdventOfCode/Year2024/Day05/Solvers/Solver.cs
@@ -0,0 +1,64 @@
+﻿using AdventOfCode.Year2024.Day05.Models;
+using AdventOfCode.Year2024.Day05.Parsers;
+using AdventOfCodeLibrary.Attributes;
+using AdventOfCodeLibrary.Parsers;
+using AdventOfCodeLibrary.Solvers;
+using System.Linq;
+
+namespace AdventOfCode.Year2024.Day05.Solvers;
+
+[Solver]
+public class Solver : BaseSolver<Manual>
+{
+    public override int Year => 2024;
+
+    public override int Day => 5;
+
+    //public override object? AnswerPartOne => ;
+
+    //public override object? AnswerPartTwo => ;
+
+    protected override IInputParser<Manual> InputParser => new ManualParser();
+
+    public override object SolvePartOne()
+    {
+        var sum = 0;
+        foreach (var update in ParsedInput.Updates)
+        {
+            var rightOrder = true;
+            for (var updateIndex = 0; updateIndex < update.Count; updateIndex++)
+            {
+                for (var beforeIndex = updateIndex - 1; beforeIndex >= 0; beforeIndex--)
+                {
+                    if (ParsedInput.Rules.Contains((update[updateIndex], update[beforeIndex])))
+                    {
+                        rightOrder = false;
+                        break;
+                    }
+                }
+                for (var afterIndex = updateIndex + 1; afterIndex < update.Count; afterIndex++)
+                {
+                    if (ParsedInput.Rules.Contains((update[afterIndex], update[updateIndex])))
+                    {
+                        rightOrder = false;
+                        break;
+                    }
+                }
+                if (!rightOrder)
+                {
+                    break;
+                }
+            }
+            if (rightOrder)
+            {
+                sum += update[update.Count / 2];
+            }
+        }
+        return sum;
+    }
+
+    public override object SolvePartTwo()
+    {
+        return 0;
+    }
+}
