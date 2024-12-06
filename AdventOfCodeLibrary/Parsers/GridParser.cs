8d3fa5cd050b9dd95d42567d81e900be8ba55837
Marcel Zwart <marcelzwart92@gmail.com>
2024-12-06 15:58:37 +0100

    Rename.

--- AdventOfCodeLibrary/Parsers/GridParser.cs
+++ AdventOfCodeLibrary/Parsers/GridParser.cs
@@ -5,15 +5,15 @@ public class GridParser : StringArrayParser, IInputParser<Grid<char>>
 {
     public new Grid<char> Parse(string input)
     {
-        var dictionary = new Grid<char>();
+        var grid = new Grid<char>();
         var parsedInput = base.Parse(input);
         for (int y = 0; y < parsedInput.Length; y++)
         {
             for (int x = 0; x < parsedInput[y].Length; x++)
             {
-                dictionary.Add(new Point2D(x, y), parsedInput[y][x]);
+                grid.Add(new Point2D(x, y), parsedInput[y][x]);
             }
         }
-        return dictionary;
+        return grid;
     }
 }
