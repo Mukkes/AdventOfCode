6d0a1cf403455a1b57f341dfd7f890bfd19e6e6f
Marcel Zwart <marcelzwart92@gmail.com>
2024-12-09 22:56:34 +0100

    Initialize year 2024 day 10.

---
+++ AdventOfCode/Year2024/Day10/Solvers/Solver.cs
@@ -0,0 +1,29 @@
+﻿using AdventOfCodeLibrary.Attributes;
+using AdventOfCodeLibrary.Parsers;
+using AdventOfCodeLibrary.Solvers;
+
+namespace AdventOfCode.Year2024.Day10.Solvers;
+
+[Solver]
+public class Solver : BaseSolver<string[]>
+{
+    public override int Year => 2024;
+
+    public override int Day => 10;
+
+    //public override object? AnswerPartOne => ;
+
+    //public override object? AnswerPartTwo => ;
+
+    protected override IInputParser<string[]> InputParser => new StringArrayParser();
+
+    public override object SolvePartOne()
+    {
+        return 0;
+    }
+
+    public override object SolvePartTwo()
+    {
+        return 0;
+    }
+}
