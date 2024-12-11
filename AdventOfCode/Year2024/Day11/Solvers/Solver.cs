ebc7b8ffafc975a4353a12cc9cadbcb751ed7421
Marcel Zwart <marcelzwart92@gmail.com>
2024-12-11 22:13:35 +0100

    Refactor year 2024 day 11.

--- AdventOfCode/Year2024/Day11/Solvers/Solver.cs
+++ AdventOfCode/Year2024/Day11/Solvers/Solver.cs
@@ -16,90 +16,52 @@ public class Solver : BaseSolver<List<long>>
     public override object? AnswerPartTwo => 264350935776416;
 
     protected override IInputParser<List<long>> InputParser => new LongListParser(" ");
+
     private Dictionary<(long stone, int blinks), long> _stonesDictionary = new Dictionary<(long stone, int blinks), long> ();
 
     public override object SolvePartOne()
     {
-        var stones = new List<long>(ParsedInput);
-        for (var i = 0; i < 25; i++)
-        {
-            var newStones = new List<long>();
-            foreach (var stone in stones)
-            {
-                newStones.AddRange(ApplyRules(stone));
-            }
-            stones = newStones;
-        }
-        return stones.Count();
+        return 
+            ParsedInput
+            .Select(stone => GetSumStone(stone, 25))
+            .Sum();
     }
 
     public override object SolvePartTwo()
     {
-        var sum = 0L;
-        foreach (var stone in new List<int>() { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9})
-        {
-            StoreBasicStones(stone, 39);
-        }
-        foreach (var stone in ParsedInput)
-        {
-            sum += ApplyRules(stone, 75);
-        }
-        return sum;
+        return
+            ParsedInput
+            .Select(stone => GetSumStone(stone, 75))
+            .Sum();
     }
 
-    private long StoreBasicStones(long beginStone, int blinks)
+    private long GetSumStone(long stone, int blinks)
     {
-        var stones = new List<long>() { beginStone };
-        for (var i = 0; i < blinks; i++)
+        if (blinks == 0)
         {
-            var newStones = new List<long>();
-            foreach (var stone in stones)
-            {
-                newStones.AddRange(ApplyRules(stone));
-            }
-            stones = newStones;
-            _stonesDictionary.Add((beginStone, i + 1), stones.Count());
+            return 1;
         }
-        return stones.Count();
-    }
-
-    private long ApplyRules(long beginStone, int blinks)
-    {
-        var sum = 0L;
-        var stones = new List<long>() { beginStone };
-        for (var i = 0; i < blinks; i++)
+        if (_stonesDictionary.TryGetValue((stone, blinks), out long value))
         {
-            var newStones = new List<long>();
-            foreach (var stone in stones)
-            {
-                if (_stonesDictionary.TryGetValue((stone, blinks - i), out long value))
-                {
-                    sum += value;
-                }
-                else
-                {
-                    newStones.AddRange(ApplyRules(stone));
-                }
-            }
-            stones = newStones;
+            return value;
         }
-        return stones.Count() + sum;
-    }
-
-    private List<long> ApplyRules(long stone)
-    {
-        var stones = new List<long>();
+        var result = 0L;
+        var stoneString = stone.ToString();
         if (stone == 0)
         {
-            return [1];
+            result = GetSumStone(1, blinks - 1);
         }
-        var stoneString = stone.ToString();
-        if (stoneString.Count() % 2 == 0)
+        else if (stoneString.Count() % 2 == 0)
         {
-            return
-                [(long.Parse(stoneString[0..(stoneString.Count() / 2)])), 
-                (long.Parse(stoneString[(stoneString.Count() / 2)..]))];
+            result = 
+                GetSumStone(long.Parse(stoneString[0..(stoneString.Count() / 2)]), blinks - 1) +
+                GetSumStone(long.Parse(stoneString[(stoneString.Count() / 2)..]), blinks - 1);
         }
-        return [stone * 2024];
+        else
+        {
+            result = GetSumStone(stone * 2024, blinks - 1);
+        }
+        _stonesDictionary.TryAdd((stone, blinks), result);
+        return result;
     }
 }
